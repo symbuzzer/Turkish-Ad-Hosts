975d3398b093633d7d4125c9a62d61309232c9e9
Ali Beyaz <symbuzzer@users.noreply.github.com>
2023-01-22 17:30:09 +0300

    Update turkish-ad-hosts-windows.bat

--- windows/turkish-ad-hosts-windows.bat
+++ windows/turkish-ad-hosts-windows.bat
@@ -2,7 +2,7 @@
 setlocal EnableDelayedExpansion
 cls
 mode con:cols=18 lines=2
-set ver=1.0.1
+set ver=1.0.0
 set name=Turkish Ad Hosts
 set title=%name% v%ver%
 set file=%~f0
@@ -13,6 +13,9 @@ set vbsfilename=%RANDOM%
 cls
 echo x=msgbox("Need Admin Rights for installing and updating Turkish Ad Hosts" + vbNewLine + " " + vbNewLine + "Note: If you don't want to see this on every boot just delete '%file%' file in '%startup%' folder" ,0, "%title%") > %temp%\%vbsfilename%.vbs
 cscript %temp%\%vbsfilename%.vbs
-xcopy /Y "%file%" "%startup%"
-echo powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/symbuzzer/Turkish-Ad-Hosts/main/hosts' -OutFile 'C:\Windows\System32\drivers\etc\hosts'" > %temp%\temp.bat
+
+echo mode con:cols=18 lines=2 > %temp%\temp.bat
+echo set startup=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup >> %temp%\temp.bat
+echo powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/symbuzzer/Turkish-Ad-Hosts/main/hosts' -OutFile 'C:\Windows\System32\drivers\etc\hosts'" >> %temp%\temp.bat
+echo powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/symbuzzer/Turkish-Ad-Hosts/main/windows/turkish-ad-hosts-windows.bat' -OutFile '%startup%\turkish-ad-hosts-windows.bat'" >> %temp%\temp.bat
 powershell -Command "Start-Process -Verb runAs -FilePath '%temp%\temp.bat'"
