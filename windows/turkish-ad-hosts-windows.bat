12f2ed259b621a9cea5ee8cb7e51387bfa25a3cf
Ali Beyaz <symbuzzer@users.noreply.github.com>
2023-01-22 17:58:52 +0300

    Update turkish-ad-hosts-windows.bat

--- windows/turkish-ad-hosts-windows.bat
+++ windows/turkish-ad-hosts-windows.bat
@@ -9,13 +9,13 @@ set file=%~f0
 set startup=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup
 title %title%
 color 0a
-set vbsfilename=%RANDOM%
+set filename=%RANDOM%
 cls
-echo x=msgbox("Need Admin Rights for installing and updating Turkish Ad Hosts" + vbNewLine + " " + vbNewLine + "Note: If you don't want to see this on every boot just delete '%file%' file in '%startup%' folder" ,0, "%title%") > %temp%\%vbsfilename%.vbs
-cscript %temp%\%vbsfilename%.vbs
+echo x=msgbox("Need Admin Rights for installing and updating Turkish Ad Hosts" + vbNewLine + " " + vbNewLine + "Note: If you don't want to see this on every boot just delete '%file%' file in '%startup%' folder" ,0, "%title%") > %temp%\%filename%.vbs
+cscript %temp%\%filename%.vbs
 
-echo mode con:cols=18 lines=2 > %temp%\temp.bat
-echo set startup=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup >> %temp%\temp.bat
-echo powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/symbuzzer/Turkish-Ad-Hosts/main/hosts' -OutFile 'C:\Windows\System32\drivers\etc\hosts'" >> %temp%\temp.bat
-echo powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/symbuzzer/Turkish-Ad-Hosts/main/windows/turkish-ad-hosts-windows.bat' -OutFile '%startup%\turkish-ad-hosts-windows.bat'" >> %temp%\temp.bat
-powershell -Command "Start-Process -Verb runAs -FilePath '%temp%\temp.bat'"
+echo mode con:cols=18 lines=2 > %temp%\%filename%.bat
+echo set startup=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup >> %temp%\%filename%.bat
+echo powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/symbuzzer/Turkish-Ad-Hosts/main/hosts' -OutFile 'C:\Windows\System32\drivers\etc\hosts'" >> %temp%\%filename%.bat
+echo powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/symbuzzer/Turkish-Ad-Hosts/main/windows/turkish-ad-hosts-windows.bat' -OutFile '%startup%\turkish-ad-hosts-windows.bat'" >> %temp%\%filename%.bat
+powershell -Command "Start-Process -Verb runAs -FilePath '%temp%\%filename%.bat'"
