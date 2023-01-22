9507e0dd427236c230623d7ac0f611ce1b34d840
Ali Beyaz <alipolatbeyaz@gmail.com>
2023-01-22 22:24:47 +0300

    Update turkish-ad-hosts-windows.bat

--- windows/turkish-ad-hosts-windows.bat
+++ windows/turkish-ad-hosts-windows.bat
@@ -6,7 +6,7 @@ set name=Turkish Ad Hosts
 set title=%name% v%ver%
 title %title%
 mode con:cols=50 lines=2
-set workingdir=%UserProfile%\tah\
+set workingdir=%UserProfile%\tah
 set file=%~f0
 set startup=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup
 title %title%
@@ -17,11 +17,11 @@ goto checkping
 
 :checkping
 ping www.google.com -n 1 -w 1000
-cls
-echo installing, please wait...
 if errorlevel 1 (goto exit) else (goto checkfirstinstall)
 
 :checkfirstinstall
+cls
+echo installing, please wait...
 if not exist "%workingdir%\version" (goto start) else (goto getversiongithub)
 
 :getversiongithub
@@ -34,13 +34,11 @@ set /p installedversion=<"%workingdir%\version"
 set /p githubversion=<"%workingdir%\version2"
 if %githubversion% gtr %installedversion% (goto start) else (goto exit)
 
-:exit
-exit
-
 :start
 cls
-echo started to install, please wait...
-echo x=msgbox("Need Admin Rights for installing and updating Turkish Ad Hosts" + vbNewLine + " " + vbNewLine + "Note: If you don't want to see this on every boot just delete '%file%' file in '%startup%' folder" ,0, "%title%") > %temp%\%filename%.vbs
+echo installing, please wait...
+if not exist "%workingdir%\version" (set message=Need Admin Rights for installing Turkish Ad Hosts) else (set message=New v%githubversion% update found! Need Admin Rights for updating Turkish Ad Hosts)
+echo x=msgbox("%message%" + vbNewLine + " " + vbNewLine + " " ,0, "%title%") > %temp%\%filename%.vbs
 cscript %temp%\%filename%.vbs
 echo set startup=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup >> %temp%\%filename%.bat
 echo powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/symbuzzer/Turkish-Ad-Hosts/main/hosts' -OutFile '%WinDir%\System32\drivers\etc\hosts'" >> %temp%\%filename%.bat
@@ -50,3 +48,7 @@ echo powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.
 powershell -Command Start-Process -Verb runAs -windowstyle hidden -Wait -FilePath '%temp%\%filename%.bat'
 cls
 echo installed!
+goto exit
+
+:exit
+exit
