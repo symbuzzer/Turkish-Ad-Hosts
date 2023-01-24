51fcdcb097774bfc4af1b3b72132019343dd865e
Ali Beyaz <symbuzzer@users.noreply.github.com>
2023-01-24 09:43:18 +0300

    Update startup.bat

--- windows/startup.bat
+++ windows/startup.bat
@@ -1,2 +1,11 @@
+@echo off
+setlocal EnableDelayedExpansion
+mode con:cols=50 lines=2
+cls
+set ver=1.0.0
+set name=Turkish Ad Hosts trigger
+set title=%name% v%ver%
+title %title%
+color 0a
 set workingdir=%UserProfile%\tah\
 powershell -Command Start-Process -windowstyle hidden -FilePath '%workingdir%\turkish-ad-hosts-windows.bat'
