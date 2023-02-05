2540631565500d177448fefe5db10f568bcafe26
Ali Beyaz <symbuzzer@users.noreply.github.com>
2023-02-05 13:41:37 +0300

    Update turkish-ad-hosts-linux.sh

--- linux/turkish-ad-hosts-linux.sh
+++ linux/turkish-ad-hosts-linux.sh
@@ -4,16 +4,19 @@ NEW_HOSTS="hosts"
 HOSTS_PATH="/etc/hosts"
 SCRIPT_URL="https://raw.githubusercontent.com/symbuzzer/Turkish-Ad-Hosts/main/linux/turkish-ad-hosts-linux.sh"
 NEW_SCRIPT="turkish-ad-hosts-linux.sh"
-PROFILED_PATH="/etc/init.d/"
+STARTUP_PATH="/etc/rc.local/"
+SCRIPT=$STARTUP_PATH$NEW_SCRIPT
 if [ "$(id -u)" -ne "0" ]; then
     echo "This script must be run as root" 1>&2
     exit 1
 fi
-sudo cp -f "$0" $PROFILED_PATH
+sudo cp -f "$0" $STARTUP_PATH
+chmod +x $SCRIPT
 sudo wget -q --tries=10 --timeout=20 --spider https://www.avalibeyaz.com
 if [[ $? -eq 0 ]]; then
     sudo wget -O $NEW_SCRIPT $SCRIPT_URL
-    sudo cp -f $NEW_SCRIPT $PROFILED_PATH
+    sudo cp -f $NEW_SCRIPT $STARTUP_PATH
+    chmod +x $SCRIPT
     sudo rm $NEW_SCRIPT*
     sudo wget -O $NEW_HOSTS $HOSTS_URL
     sudo cp -f $NEW_HOSTS $HOSTS_PATH
