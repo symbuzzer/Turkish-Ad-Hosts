9e3af9a369bb12fe4e8ef9c4dca12223aa3ec1b1
Ali Beyaz <symbuzzer@users.noreply.github.com>
2023-02-05 12:05:30 +0300

    Update uninstall.bat

--- windows/uninstall.bat
+++ windows/uninstall.bat
@@ -7,18 +7,18 @@ set name=Turkish Ad Hosts uninstaller
 set title=%name% v%ver%
 title %title%
 color 0a
-set workingdir=%UserProfile%\tah
-set startupfile=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\startup.bat
-set shortcutfolder=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Turkish-Ad-Hosts
+set workingdir="%UserProfile%\tah"
+set startupfile="%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\startup.bat"
+set shortcutfolder="%APPDATA%\Microsoft\Windows\Start Menu\Programs\Turkish-Ad-Hosts"
 echo.welcome to %name% v%ver%
 goto delete
 
 :delete
 echo.starting uninstalling...
-del %workingdir%
-del %startupfile%
-del %shortcutfolder%
-exit
+rmdir /s /q %workingdir%
+del /f /q %startupfile%
+rmdir /s /q %shortcutfolder%
+goto exit
 
 :exit
 echo.uninstalled succesfully!
