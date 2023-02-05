e060a7c0f5d59b81106ab8f0756080f7a3dc15cf
Ali Beyaz <symbuzzer@users.noreply.github.com>
2023-02-05 12:56:42 +0300

    Update turkish-ad-hosts-windows.bat

--- windows/turkish-ad-hosts-windows.bat
+++ windows/turkish-ad-hosts-windows.bat
@@ -2,15 +2,18 @@
 setlocal EnableDelayedExpansion
 mode con:cols=50 lines=2
 cls
-set ver=1.2.1
+set ver=1.2.2
 set name=Turkish Ad Hosts
 set title=%name% v%ver%
 title %title%
 color 0a
 set workingdir=%UserProfile%\tah
+mkdir "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Turkish-Ad-Hosts"
 set startup=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup
-set shortcutfile=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Turkish-Ad-Hosts.lnk
+set shortcutfile=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Turkish-Ad-Hosts\Turkish-Ad-Hosts.lnk
 set launcherfile=%UserProfile%\tah\launcher.bat
+set uninstallshortcutfile=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Turkish-Ad-Hosts\Uninstall.lnk
+set uninstallerfile=%UserProfile%\tah\uninstall.bat
 set filename=%RANDOM%
 set filename2=%RANDOM%
 set filename3=%RANDOM%
@@ -45,6 +48,7 @@ echo powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.
 echo powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/symbuzzer/Turkish-Ad-Hosts/main/windows/turkish-ad-hosts-windows.bat' -OutFile '%workingdir%\turkish-ad-hosts-windows.bat'" >> %temp%\%filename%.bat
 echo powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/symbuzzer/Turkish-Ad-Hosts/main/windows/startup.bat' -OutFile '%startup%\startup.bat'" >> %temp%\%filename%.bat
 echo powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/symbuzzer/Turkish-Ad-Hosts/main/windows/launcher.bat' -OutFile '%workingdir%\launcher.bat'" >> %temp%\%filename%.bat
+echo powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/symbuzzer/Turkish-Ad-Hosts/main/windows/uninstall.bat' -OutFile '%workingdir%\uninstall.bat'" >> %temp%\%filename%.bat
 echo powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/symbuzzer/Turkish-Ad-Hosts/main/version' -OutFile '%workingdir%\version'" >> %temp%\%filename%.bat
 powershell -Command Start-Process -Verb runAs -windowstyle hidden -Wait -FilePath '%temp%\%filename%.bat'
 cls
@@ -61,6 +65,10 @@ echo Set oWS = WScript.CreateObject("WScript.Shell") > %temp%\%filename3%.vbs
 echo sLinkFile = "%shortcutfile%" >> %temp%\%filename3%.vbs
 echo Set oLink = oWS.CreateShortcut(sLinkFile) >> %temp%\%filename3%.vbs
 echo oLink.TargetPath = "%launcherfile%" >> %temp%\%filename3%.vbs
+echo oLink.Save >> %temp%\%filename3%.vbs
+echo sLinkFile = "%uninstallshortcutfile%" >> %temp%\%filename3%.vbs
+echo Set oLink = oWS.CreateShortcut(sLinkFile) >> %temp%\%filename3%.vbs
+echo oLink.TargetPath = "%uninstallerfile%" >> %temp%\%filename3%.vbs
 echo oLink.Save >> %temp%\%filename3%.vbs
 cscript %temp%\%filename3%.vbs
 goto exit
