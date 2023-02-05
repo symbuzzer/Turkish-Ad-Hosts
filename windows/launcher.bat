e464b16f52cffe2e49d5ba22ea9c1889e7317f87
Ali Beyaz <symbuzzer@users.noreply.github.com>
2023-02-05 11:56:56 +0300

    Update launcher.bat

--- windows/launcher.bat
+++ windows/launcher.bat
@@ -1,46 +1,80 @@
 @echo off
 setlocal EnableDelayedExpansion
-mode con:cols=70 lines=10
+mode con:cols=50 lines=2
 cls
-set ver=1.0.1
-set name=Turkish Ad Hosts launcher
+set ver=1.2.2
+set name=Turkish Ad Hosts
 set title=%name% v%ver%
 title %title%
 color 0a
-set workingdir=%UserProfile%\tah\
-echo.welcome to %name% v%ver%
+set workingdir=%UserProfile%\tah
+mkdir "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Turkish-Ad-Hosts"
+set startup=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup
+set shortcutfile=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Turkish-Ad-Hosts\Turkish-Ad-Hosts.lnk
+set launcherfile=%UserProfile%\tah\launcher.bat
+set uninstallshortcutfile=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Turkish-Ad-Hosts\Uninstall.lnk
+set uninstallerfile=%UserProfile%\tah\uninstall.bat
+set filename=%RANDOM%
+set filename2=%RANDOM%
+set filename3=%RANDOM%
+if not exist "%workingdir%" mkdir "%workingdir%"
 goto checkping
-
 
 :checkping
 ping www.avalibeyaz.com -n 1 -w 1000 >nul
-echo checking connection, please wait...
-if errorlevel 1 (goto neterror) else (goto getversiongithub)
+cls
+echo installing, please wait...
+if errorlevel 1 (goto exit) else (goto checkfirstinstall)
+
+:checkfirstinstall
+if not exist "%workingdir%\version" (goto start) else (goto getversiongithub)
 
 :getversiongithub
-echo powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/symbuzzer/Turkish-Ad-Hosts/main/version' -OutFile '%workingdir%\version2'" >> %temp%\getversiongithub.bat
-powershell -Command Start-Process -windowstyle hidden -Wait -FilePath '%temp%\getversiongithub.bat'
+echo powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/symbuzzer/Turkish-Ad-Hosts/main/version' -OutFile '%workingdir%\version2'" >> %temp%\%filename2%.bat
+powershell -Command Start-Process -windowstyle hidden -Wait -FilePath '%temp%\%filename2%.bat'
 goto compare
 
 :compare
 set /p installedversion=<"%workingdir%\version"
 set /p githubversion=<"%workingdir%\version2"
-echo.online version: %githubversion% - installed version: %installedversion%
-if %githubversion% gtr %installedversion% (goto start) else (goto noupdate)
+if %githubversion% gtr %installedversion% (goto start) else (goto exit)
 
 :start
-echo.update found and will be installed
-powershell -Command Start-Process -windowstyle hidden -FilePath '%workingdir%\turkish-ad-hosts-windows.bat'
-echo.starting...
-timeout /T 5 >nul
-exit
+cls
+if not exist "%workingdir%\version" (set message=Welcome to Turkish Ad Hosts) else (set message=New v%githubversion% update found!)
+echo x=msgbox("%message%" + vbNewLine + " " + vbNewLine + "Need Admin Rights for installation process" ,0, "%title%") > %temp%\%filename%.vbs
+cscript %temp%\%filename%.vbs
+echo powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/symbuzzer/Turkish-Ad-Hosts/main/hosts' -OutFile '%WinDir%\System32\drivers\etc\hosts'" >> %temp%\%filename%.bat
+echo powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/symbuzzer/Turkish-Ad-Hosts/main/windows/turkish-ad-hosts-windows.bat' -OutFile '%workingdir%\turkish-ad-hosts-windows.bat'" >> %temp%\%filename%.bat
+echo powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/symbuzzer/Turkish-Ad-Hosts/main/windows/startup.bat' -OutFile '%startup%\startup.bat'" >> %temp%\%filename%.bat
+echo powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/symbuzzer/Turkish-Ad-Hosts/main/windows/launcher.bat' -OutFile '%workingdir%\launcher.bat'" >> %temp%\%filename%.bat
+echo powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/symbuzzer/Turkish-Ad-Hosts/main/windows/uninstall.bat' -OutFile '%workingdir%\uninstall.bat'" >> %temp%\%filename%.bat
+echo powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/symbuzzer/Turkish-Ad-Hosts/main/version' -OutFile '%workingdir%\version'" >> %temp%\%filename%.bat
+powershell -Command Start-Process -Verb runAs -windowstyle hidden -Wait -FilePath '%temp%\%filename%.bat'
+cls
+goto checkfirstinstalled
 
-:noupdate
-echo.no update found :(
-pause
-exit
+:checkfirstinstalled
+if not exist "%workingdir%\version" (goto exit) else (goto checkshortcut)
 
-:neterror
-echo.need internet connection :(
-pause
+:checkshortcut
+if not exist "%shortcutfile%" (goto createshortcut) else (goto exit)
+
+:createshortcut
+echo Set oWS = WScript.CreateObject("WScript.Shell") > %temp%\%filename3%.vbs
+echo sLinkFile = "%shortcutfile%" >> %temp%\%filename3%.vbs
+echo Set oLink = oWS.CreateShortcut(sLinkFile) >> %temp%\%filename3%.vbs
+echo oLink.TargetPath = "%launcherfile%" >> %temp%\%filename3%.vbs
+echo oLink.Save >> %temp%\%filename3%.vbs
+echo sLinkFile = "%uninstallshortcutfile%" >> %temp%\%filename3%.vbs
+echo Set oLink = oWS.CreateShortcut(sLinkFile) >> %temp%\%filename3%.vbs
+echo oLink.TargetPath = "%uninstallerfile%" >> %temp%\%filename3%.vbs
+echo oLink.Save >> %temp%\%filename3%.vbs
+cscript %temp%\%filename3%.vbs
+goto exit
+
+:noconnection
+goto exit
+
+:exit
 exit
