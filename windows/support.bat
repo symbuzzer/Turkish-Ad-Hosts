a7718a1d4a44bb1021ccbd6d20cc68bfd574ad6b
Ali Beyaz <symbuzzer@users.noreply.github.com>
2023-02-18 11:55:03 +0300

    Create support.bat

---
+++ windows/support.bat
@@ -0,0 +1,10 @@
+@echo off
+setlocal EnableDelayedExpansion
+mode con:cols=50 lines=2
+cls
+set ver=1.0.0
+set name=Turkish Ad Hosts support
+set title=%name% v%ver%
+title %title%
+color 0a
+start "" "https://avalibeyaz.com/patreon"
