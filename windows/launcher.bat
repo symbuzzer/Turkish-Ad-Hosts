e96d9599b092736e26b8219e73ca198c29de7231
Ali Beyaz <symbuzzer@users.noreply.github.com>
2023-02-18 12:22:32 +0300

    Add files via upload

--- windows/launcher.bat
+++ windows/launcher.bat
@@ -2,15 +2,17 @@
 setlocal EnableDelayedExpansion
 mode con:cols=70 lines=10
 cls
-set ver=1.0.2
+set ver=1.0.3
 set name=Turkish Ad Hosts launcher
 set title=%name% v%ver%
 title %title%
 color 0a
 set workingdir=%UserProfile%\tah\
 echo.Welcome to %name% v%ver%
-goto checkping
+goto checkversionfile
 
+:checkversionfile
+if exist "%workingdir%\version" (goto checkping) else (goto runinstall)
 
 :checkping
 ping www.avalibeyaz.com -n 1 -w 100 >nul
@@ -32,6 +34,9 @@ if %githubversion% gtr %installedversion% (goto start) else (goto noupdate)
 
 :start
 echo.Update found and will be installed...
+goto runinstall
+
+:runinstall
 powershell -Command Start-Process -windowstyle hidden -FilePath '%workingdir%\turkish-ad-hosts-windows.bat'
 echo.starting...
 timeout /T 5 >nul
