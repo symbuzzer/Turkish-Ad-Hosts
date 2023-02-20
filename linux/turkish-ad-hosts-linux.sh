c58c719442d30aedfba80ca72b160a1a60dfc9fa
Ali Beyaz <symbuzzer@users.noreply.github.com>
2023-02-20 12:26:00 +0300

    Add files via upload

--- linux/turkish-ad-hosts-linux.sh
+++ linux/turkish-ad-hosts-linux.sh
@@ -2,22 +2,11 @@
 HOSTS_URL="https://raw.githubusercontent.com/symbuzzer/Turkish-Ad-Hosts/main/hosts"
 NEW_HOSTS="hosts"
 HOSTS_PATH="/etc/"
-SCRIPT_URL="https://raw.githubusercontent.com/symbuzzer/Turkish-Ad-Hosts/main/linux/turkish-ad-hosts-linux.sh"
-NEW_SCRIPT="turkish-ad-hosts-linux.sh"
-STARTUP_PATH="/etc/rc.local/"
-SCRIPT=$STARTUP_PATH$NEW_SCRIPT
 if [ "$(id -u)" -ne "0" ]; then
     echo "This script must be run as root" 1>&2
     exit 1
 fi
-sudo cp -f "$0" $STARTUP_PATH
-chmod +x $SCRIPT
-sudo wget -q --tries=5 --timeout=10 --spider https://www.avalibeyaz.com
-if [[ $? -eq 0 ]]; then
-    sudo wget -O $NEW_SCRIPT $SCRIPT_URL
-    sudo cp -f $NEW_SCRIPT $STARTUP_PATH
-    chmod +x $SCRIPT
-    sudo rm $NEW_SCRIPT*
+if nc -zw1 google.com 443; then
     sudo wget -O $NEW_HOSTS $HOSTS_URL
     sudo cp -f $NEW_HOSTS $HOSTS_PATH
     sudo rm $NEW_HOSTS*
