9ba75320f4cdc7f7ef0388773179a154846f295d
Ali Beyaz <symbuzzer@users.noreply.github.com>
2023-03-21 16:03:22 +0300

    Add files via upload

--- windows/startup.bat
+++ windows/startup.bat
@@ -2,10 +2,33 @@
 setlocal EnableDelayedExpansion
 mode con:cols=50 lines=2
 cls
-set ver=1.0.0
+set ver=1.1.0
 set name=Turkish Ad Hosts trigger
 set title=%name% v%ver%
 title %title%
 color 0a
 set workingdir=%UserProfile%\tah\
+goto checkPrivileges
+
+:checkPrivileges
+cls
+NET FILE 1>NUL 2>NUL
+if '%errorlevel%' == '0' ( goto gotPrivileges ) else ( goto getPrivileges )
+
+:getPrivileges
+if '%1'=='ELEV' (shift & goto gotPrivileges)
+setlocal DisableDelayedExpansion
+set "batchPath=%~0"
+setlocal EnableDelayedExpansion
+ECHO Set UAC = CreateObject^("Shell.Application"^) > "%temp%\OEgetPrivileges.vbs"
+ECHO UAC.ShellExecute "!batchPath!", "ELEV", "", "runas", 1 >> "%temp%\OEgetPrivileges.vbs"
+"%temp%\OEgetPrivileges.vbs"
+exit /B
+
+:gotPrivileges
+setlocal & pushd .
+echo.get admin rights succesfully
+goto startup
+
+:startup
 powershell -Command Start-Process -windowstyle hidden -FilePath '%workingdir%\turkish-ad-hosts-windows.bat'
--- windows/turkish-ad-hosts-windows.bat
+++ windows/turkish-ad-hosts-windows.bat
@@ -1,8 +1,8 @@
 @echo off
 setlocal EnableDelayedExpansion
-mode con:cols=50 lines=2
+mode con:cols=55 lines=7
 cls
-set ver=1.2.4
+set ver=2.0.0
 set name=Turkish Ad Hosts
 set title=%name% v%ver%
 title %title%
@@ -10,72 +10,136 @@ color 0a
 set workingdir=%UserProfile%\tah
 mkdir "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Turkish-Ad-Hosts"
 set startup=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup
+set startupfile="%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\startup.bat"
+set mainfile=%UserProfile%\tah\turkish-ad-hosts-windows.bat
+set shortcutfolder="%APPDATA%\Microsoft\Windows\Start Menu\Programs\Turkish-Ad-Hosts"
 set shortcutfile=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Turkish-Ad-Hosts\Turkish-Ad-Hosts.lnk
-set launcherfile=%UserProfile%\tah\launcher.bat
 set supportshortcutfile=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Turkish-Ad-Hosts\Support.lnk
-set supportfile=%UserProfile%\tah\support.bat
 set uninstallshortcutfile=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Turkish-Ad-Hosts\Uninstall.lnk
-set uninstallerfile=%UserProfile%\tah\uninstall.bat
 set filename=%RANDOM%
 set filename2=%RANDOM%
 set filename3=%RANDOM%
+set filename4=%RANDOM%
 if not exist "%workingdir%" mkdir "%workingdir%"
+set versionfile="%workingdir%\version"
+set version2file="%workingdir%\version2"
+goto maintask
+
+:maintask
+cls
+echo.start arg: %~1
+goto commandlinearg
+
+:commandlinearg
+if "%~1"=="-support" goto support
+if "%~1"=="-uninstall" goto uninstall
+if "%~1"=="" goto checkPrivileges
+
+:checkPrivileges
+cls
+NET FILE 1>NUL 2>NUL
+if '%errorlevel%' == '0' ( goto gotPrivileges ) else ( goto getPrivileges )
+
+:getPrivileges
+if '%1'=='ELEV' (shift & goto gotPrivileges)
+setlocal DisableDelayedExpansion
+set "batchPath=%~0"
+setlocal EnableDelayedExpansion
+ECHO Set UAC = CreateObject^("Shell.Application"^) > "%temp%\OEgetPrivileges.vbs"
+ECHO UAC.ShellExecute "!batchPath!", "ELEV", "", "runas", 1 >> "%temp%\OEgetPrivileges.vbs"
+"%temp%\OEgetPrivileges.vbs"
+exit /B
+
+:gotPrivileges
+setlocal & pushd .
+echo.get admin rights succesfully
+goto cleanoldinstalls
+
+:cleanoldinstalls
+if exist "%workingdir%\launcher.bat" del /f /q "%workingdir%\launcher.bat"
+if exist "%workingdir%\support.bat" del /f /q "%workingdir%\support.bat"
+if exist "%workingdir%\uninstall.bat" del /f /q "%workingdir%\uninstall.bat"
 goto checkping
 
 :checkping
+echo.checking internet connection...
 ping www.google.com -n 1 -w 1000 >nul
-cls
-echo installing, please wait...
-if errorlevel 1 (goto exit) else (goto checkfirstinstall)
+if errorlevel 1 (goto nointernet) else (goto checkfirstinstall)
 
 :checkfirstinstall
-if not exist "%workingdir%\version" (goto start) else (goto getversiongithub)
+if not exist %versionfile% (goto start) else (goto getversiongithub)
 
 :getversiongithub
-echo powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/symbuzzer/Turkish-Ad-Hosts/main/version' -OutFile '%workingdir%\version2'" >> %temp%\%filename2%.bat
+echo.checking update...
+echo powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/symbuzzer/Turkish-Ad-Hosts/main/version' -OutFile '%version2file%'" >> %temp%\%filename2%.bat
 powershell -Command Start-Process -windowstyle hidden -Wait -FilePath '%temp%\%filename2%.bat'
 goto compare
 
 :compare
-set /p installedversion=<"%workingdir%\version"
-set /p githubversion=<"%workingdir%\version2"
-if %githubversion% gtr %installedversion% (goto start) else (goto exit)
+set /p installedversion=<%versionfile%
+set /p githubversion=<%version2file%
+if %githubversion% gtr %installedversion% (goto start) else (goto noupdatefound)
 
 :start
-cls
-if not exist "%workingdir%\version" (set message=Welcome to Turkish Ad Hosts) else (set message=New v%githubversion% update found!)
-echo x=msgbox("%message%" + vbNewLine + " " + vbNewLine + "Need Admin Rights for installation process" ,0, "%title%") > %temp%\%filename%.vbs
-cscript %temp%\%filename%.vbs
+echo.getting sources from github...
 echo powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/symbuzzer/Turkish-Ad-Hosts/main/hosts' -OutFile '%WinDir%\System32\drivers\etc\hosts'" >> %temp%\%filename%.bat
 echo powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/symbuzzer/Turkish-Ad-Hosts/main/windows/turkish-ad-hosts-windows.bat' -OutFile '%workingdir%\turkish-ad-hosts-windows.bat'" >> %temp%\%filename%.bat
 echo powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/symbuzzer/Turkish-Ad-Hosts/main/windows/startup.bat' -OutFile '%startup%\startup.bat'" >> %temp%\%filename%.bat
-echo powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/symbuzzer/Turkish-Ad-Hosts/main/windows/launcher.bat' -OutFile '%workingdir%\launcher.bat'" >> %temp%\%filename%.bat
-echo powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/symbuzzer/Turkish-Ad-Hosts/main/windows/uninstall.bat' -OutFile '%workingdir%\uninstall.bat'" >> %temp%\%filename%.bat
-echo powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/symbuzzer/Turkish-Ad-Hosts/main/windows/support.bat' -OutFile '%workingdir%\support.bat'" >> %temp%\%filename%.bat
-echo powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/symbuzzer/Turkish-Ad-Hosts/main/version' -OutFile '%workingdir%\version'" >> %temp%\%filename%.bat
+echo powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/symbuzzer/Turkish-Ad-Hosts/main/version' -OutFile '%versionfile%'" >> %temp%\%filename%.bat
 powershell -Command Start-Process -Verb runAs -windowstyle hidden -Wait -FilePath '%temp%\%filename%.bat'
-cls
+echo.installed succesfully
 goto checkfirstinstalled
 
 :checkfirstinstalled
-if not exist "%workingdir%\version" (goto exit) else (goto createshortcut)
+if not exist %versionfile% (goto exit) else (goto createshortcut)
 
 :createshortcut
 echo Set oWS = WScript.CreateObject("WScript.Shell") > %temp%\%filename3%.vbs
 echo sLinkFile = "%shortcutfile%" >> %temp%\%filename3%.vbs
 echo Set oLink = oWS.CreateShortcut(sLinkFile) >> %temp%\%filename3%.vbs
-echo oLink.TargetPath = "%launcherfile%" >> %temp%\%filename3%.vbs
+echo oLink.TargetPath = "%mainfile%" >> %temp%\%filename3%.vbs
 echo oLink.Save >> %temp%\%filename3%.vbs
 echo sLinkFile = "%supportshortcutfile%" >> %temp%\%filename3%.vbs
 echo Set oLink = oWS.CreateShortcut(sLinkFile) >> %temp%\%filename3%.vbs
-echo oLink.TargetPath = "%supportfile%" >> %temp%\%filename3%.vbs
+echo oLink.TargetPath = "%mainfile%" >> %temp%\%filename3%.vbs
+echo oLink.Arguments = "-support" >> %temp%\%filename3%.vbs
 echo oLink.Save >> %temp%\%filename3%.vbs
 echo sLinkFile = "%uninstallshortcutfile%" >> %temp%\%filename3%.vbs
 echo Set oLink = oWS.CreateShortcut(sLinkFile) >> %temp%\%filename3%.vbs
-echo oLink.TargetPath = "%uninstallerfile%" >> %temp%\%filename3%.vbs
+echo oLink.TargetPath = "%mainfile%" >> %temp%\%filename3%.vbs
+echo oLink.Arguments = "-uninstall" >> %temp%\%filename3%.vbs
 echo oLink.Save >> %temp%\%filename3%.vbs
-cscript %temp%\%filename3%.vbs
+cscript %temp%\%filename3%.vbs >nul
+echo.start menu shortcuts created succesfully
 goto exit
 
+:support
+start "" "https://avalibeyaz.com/patreon"
+goto directexit
+
+:uninstall
+echo.starting uninstalling...
+del /f /q %startupfile% > nul 2> nul
+rmdir /s /q %shortcutfolder% > nul 2> nul
+if exist %versionfile% del /f /q %versionfile%
+if exist %version2file% del /f /q %version2file%
+echo powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/symbuzzer/Turkish-Ad-Hosts/main/windows/defaulthosts' -OutFile '%WinDir%\System32\drivers\etc\hosts'" >> %temp%\%filename4%.bat
+powershell -Command Start-Process -Verb runAs -windowstyle hidden -Wait -FilePath '%temp%\%filename4%.bat' > nul 2> nul
+echo.uninstalled succesfully
+goto exit
+
+:nointernet
+echo.no internet connection :(
+goto exit
+
+:noupdatefound
+echo.no update found :)
+goto exit
+
+:directexit
+exit
+
 :exit
+echo.press any key to exit
+pause >nul
 exit
